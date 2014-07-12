1a657bf0f46842dab2bd1ac478276025e35eedd4
Gregory Daynes <gregdaynes@gmail.com>
2014-07-12 01:04:30 -0700

    Sample config

---
+++ v1/configuration.dist.php
@@ -0,0 +1,10 @@
+<?php
+
+class Configuration
+{
+
+  var $dev_mode = false; // boolean
+  var $access_url = 'api.example.com'; // url calling the api - no http:// or https://
+  var $cache_time = 5; // length of time before rewriting files
+
+}
